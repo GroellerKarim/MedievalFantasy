52fe1c7a265a2b9917473ef6bd2f599ce7a1d304
GroellerKarim <karim.jonas.groeller@gmail.com>
2022-11-05 13:39:35 +0100

    Initial Commit
     Changes to be committed:
    	new file:   .github/workflows/build.yml
    	new file:   .gitignore
    	new file:   README.md
    	new file:   build.gradle
    	new file:   gradle.properties
    	new file:   gradle/wrapper/gradle-wrapper.jar
    	new file:   gradle/wrapper/gradle-wrapper.properties
    	new file:   gradlew
    	new file:   gradlew.bat
    	new file:   settings.gradle
    	new file:   src/main/java/eu/kekx/medievalfantasy/MedievalFantasyMod.java
    	new file:   src/main/java/eu/kekx/medievalfantasy/MedievalModClient.java
    	new file:   src/main/java/eu/kekx/medievalfantasy/mixin/ExampleMixin.java
    	new file:   src/main/resources/fabric.mod.json
    	new file:   src/main/resources/medievalfantasy.mixins.json
    
    Signed-Off-By: Karim Jonas Groeller <groeller@kekx.eu>

---
+++ src/main/java/eu/kekx/medievalfantasy/MedievalModClient.java
@@ -0,0 +1,10 @@
+package eu.kekx.medievalfantasy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MedievalModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
